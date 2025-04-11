36298eff34baea8d34c0379cd7fb9587d1006896
marcelsmeets511 <marcelsmeets511@gmail.com>
2025-04-11 20:09:08 +0200

    updated frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -29,5 +29,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // Dit is optioneel en kan worden verwijderd als je het niet gebruikt.
-reportWebVitals();
+//reportWebVitals();
 
